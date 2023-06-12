6943d07290b50a493733eb2fb285065d79e2ce29
rosiekg123 <rosiekg1237@googlemail.com>
2023-06-12 21:25:52 +0100

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import "./app.css";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
 import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
 export default function App() {
   return (
