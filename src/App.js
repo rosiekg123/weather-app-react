10ac48e90fe60c1c48ae7de7d64a02dc50e80cac
rosiekg123 <rosiekg1237@googlemail.com>
2023-06-12 21:31:03 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./styles.css";
 import Weather from "./Weather";
@@ -6,28 +5,10 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
 
 export default App;
-
-return (
-  <div className="App">
-    <h1>Weather App</h1>
-    <Weather />
-  </div>
-);
